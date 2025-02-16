42631e80fe82370394b040b1dbd45879587b2d5e
Pulkit <ahujapulkit0202@gmail.com>
2025-02-16 12:01:05 +0530

    Fixed base path for local & deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/SportMateReactFrontEnd/",  // Add this line
   plugins: [react()],
-})
+  base: process.env.NODE_ENV === "production" ? "/SportMateReactFrontEnd/" : "/", // Fix for local & deploy
+});
